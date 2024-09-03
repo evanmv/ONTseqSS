e1873174cddafa6732f644c47d6c496dbd231b19
ec-evanmv <evanmv@uio.no>
2024-09-03 13:25:52 +0200

    Finished feature counts, made interactive table and wrote out counts and stats data

--- featureCounts.R
+++ featureCounts.R
@@ -10,7 +10,9 @@ library(Rsubread)
 library(tidyverse)
 library(annotate)
 library(org.Hs.eg.db)
-
+library(gt)
+library(plotly)
+library(DT)
 ## featureCounts
 fC <- featureCounts(files = c("alignment.sam"),
                     annot.inbuilt = "hg38",
@@ -23,6 +25,7 @@ fC_counts <- as.data.frame(fC$counts)
 
 head(fC_counts)
 fC$stat
+head(fC$annotation)
 row_names <- dimnames(fC_counts)[[1]]
 print(row_names)
 
@@ -38,6 +41,36 @@ countsTidy <-
   countsTidy %>%
   mutate(Symbol = Symbols, .before=1)
 
+countsTidy$Symbol[countsTidy$Symbol == 0] <- NA
+
+countsTidy <- na.exclude(countsTidy) 
+
+colnames(countsTidy)[3] <- "Raw counts"
+
+#Excel readable file
 write_csv(countsTidy, "geneCounts.csv")
 
+#Static table - Not relevant without filtering
+countsTidy %>%
+  gt() %>%
+  tab_header(
+    title = "Raw Mapped Reads for OTM_CTRL_24T_2024"
+  ) %>%
+  tab_footnote(
+    footnote = "Reads mapped with minimap2 v2.26"
+  )
 
+#Interactive table
+datatable(countsTidy,
+          extensions = c("KeyTable", "FixedHeader"),
+          filter = "top",
+          options = list(keys = TRUE,
+                         searchHighlight = TRUE,
+                         pageLength = 10, 
+                         lengthMenu = c("10", "25", "50", "100")),
+          caption = "Raw Mapped Reads for OTM_CTRL_24T_2024"
+          )
+
+?datatable
+
+write_csv(fC$stat, "alignmentStats.csv")
